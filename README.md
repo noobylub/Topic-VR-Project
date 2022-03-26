7a67b76b0aa76b28564dc83ecd43031a4ae7f410
noobylub <73588764+noobylub@users.noreply.github.com>
2022-03-26 17:59:21 +0300

    first and almost done commit

--- README.md
+++ README.md
@@ -4,19 +4,9 @@ Boilerplate for building virtual reality experiences with
 [A-Frame](https://aframe.io) and React with
 [aframe-react](https://github.com/ngokevin/aframe-react).
 
-![aframe-react-boilerplate](https://cloud.githubusercontent.com/assets/674727/24401414/286adeec-1369-11e7-8c41-22810d22d8d0.png)
-
-### Installation
-
-To get started:
-
-```bash
-npm install
-npm start
-```
-
-To publish to GitHub Pages:
-
-```bash
-npm run publish
-```
+React boilerplate from ngokevin
+https://github.com/ngokevin/aframe-react-boilerplate.git
+Project idea from 
+https://medium.com/@nrs007/designing-another-world-creating-a-vr-escape-room-scavenger-hunt-game-c11493f4fc73
+Timer from 
+https://betterprogramming.pub/building-a-simple-countdown-timer-with-react-4ca32763dda7
--- src/index.js
+++ src/index.js
@@ -1,57 +1,252 @@
-import 'aframe';
-import 'aframe-animation-component';
-import 'aframe-particle-system-component';
-import 'babel-polyfill';
-import {Entity, Scene} from 'aframe-react';
-import React from 'react';
-import ReactDOM from 'react-dom';
+//Starting with credits
+//A frame boiler plate from ngokevin
+
+import "aframe";
+import "aframe-animation-component";
+import "aframe-particle-system-component";
+import "babel-polyfill";
+import { Entity, Scene } from "aframe-react";
+import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {color: 'red'};
+    this.displayRandom = [];
+    this.findThis = [];
+    
+    //where the data are
+    this.state = {
+      remaining: 5,
+      displayData: this.displayRandom,
+      toFind: this.findThis,
+      color: "red",
+      colors: ["red", "orange", "yellow", "green", "blue"],
+      minutes: 1,
+      seconds: 0,
+      minuteComp: 0,
+      secondComp: 0,
+      succeed : false,
+      ypos: [this.randomXpos(3,1),this.randomXpos(3,1),this.randomXpos(3,1),this.randomXpos(3,1),this.randomXpos(3,1)],
+     
+    };
   }
 
-  changeColor() {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+  
+  //Where I will set up the components
+  componentDidMount() {
+    for (let i = 0; i < 101; i++) {
+      this.displayRandom.push(
+        <Entity
+          id="box"
+          // animation__rotate={{
+          //   property: "rotation",
+          //   dur: 2000,
+          //   loop: true,
+          //   to: "360 360 360",
+          // }}
+          geometry={{ primitive: "box", roughness: 0.5 }}
+          material={{ color: this.state.color, opacity: 0.9 }}
+          position={{
+            x: this.randomXpos(20, -20),
+            y: this.randomXpos(3, 1),
+            z: this.randomXpos(20, -20),
+          }}
+        ></Entity>
+      );
+     
+    }
+    for(let i =0;i<5;i++){
+      
+      this.findThis.push(
+        <Entity
+          id="box"
+          key = {i}
+          geometry={{ primitive: "box", width: 0.5, height: 0.5 }}
+          material={{ color: "green", opacity: 1 }}
+          position={{
+            x: this.randomXpos(10, -10),
+            y: this.state.ypos[i],
+            z: this.randomXpos(10, -10),
+          }}
+          events={{
+            click: () => {
+              console.log("working")
+              
+              
+              if(this.state.ypos[i] !== -99){
+                this.setState({
+                  remaining : this.state.remaining -1
+                })
+              }
+              else{
+                this.changeColor();
+              }
+              if(this.state.remaining === 0){
+                this.setState({
+                  minuteComp : this.state.minutes,
+                  secondComp: this.state.seconds,
+                  succeed : true
+                })
+              }
+              let array = this.state.ypos;
+              array[i] = -99;
+              this.setState({
+                ypos : array
+              })
+              console.log(this.state.remaining);
+            },
+          }}
+        ></Entity>
+      );
+    }
     this.setState({
-      color: colors[Math.floor(Math.random() * colors.length)]
+      toFind: this.findThis,
+      displayData: this.displayRandom,
     });
   }
 
-  render () {
+  changeColor() {
+    this.setState({
+      color:
+        this.state.colors[Math.floor(Math.random() * this.state.colors.length)],
+    });
+  }
+
+  randomXpos(max, min) {
+    return Math.random() * (max - min + 1) + min;
+  }
+
+  //timer function
+  timerStart() {
+    this.myInterval = setInterval(() => {
+      const { seconds, minutes } = this.state;
+      if (seconds > 0) {
+        this.setState(({ seconds }) => ({
+          seconds: seconds - 1,
+        }));
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(this.myInterval);
+        } else {
+          this.setState(({ minutes }) => ({
+            minutes: minutes - 1,
+            seconds: 59,
+          }));
+        }
+      }
+    }, 1000);
+  }
+  
+
+  render() {
     return (
       <Scene>
+        {/* Loading the assets */}
         <a-assets>
-          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
-          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
+          <img
+            id="groundTexture"
+            src="https://cdn.aframe.io/a-painter/images/floor.jpg"
+          />
+          <img
+            id="skyTexture"
+            src="https://cdn.aframe.io/a-painter/images/sky.jpg"
+          />
         </a-assets>
 
-        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
-        <Entity primitive="a-light" type="ambient" color="#445451"/>
-        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
-        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
-        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+        {/* The backgrounds */}
+        <Entity
+          primitive="a-plane"
+          src="#groundTexture"
+          rotation="-90 0 0"
+          height="100"
+          material={{ color: this.state.color }}
+          width="100"
+        />
+        <Entity primitive="a-light" type="ambient" color="#445451" />
+        <Entity
+          primitive="a-light"
+          type="point"
+          intensity="2"
+          position="2 4 4"
+        />
+        <Entity
+          primitive="a-sky"
+          height="2048"
+          radius="30"
+          src="#skyTexture"
+          theta-length="90"
+          width="2048"
+        />
+        <Entity particle-system={{ preset: "snow", particleCount: 2000 }} />
 
-        <Entity id="box"
-          geometry={{primitive: 'box'}}
-          material={{color: this.state.color, opacity: 0.6}}
-          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-          position={{x: 0, y: 1, z: -3}}
-          events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
-                  geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
-                  material={{color: '#24CAFF'}}/>
-        </Entity>
+        {/* Main Text */}
+        <Entity
+          text={{
+            value: `You need to find ${this.state.remaining} more red squares`,
+            align: "center",
+          }}
+          position={{ x: 0, y: 3, z: -1 }}
+        />
+        <Entity
+          text={{
+            value: `You have ${this.state.minutes} Minute and ${
+              this.state.seconds > 10
+                ? `${this.state.seconds} Seconds`
+                : `${this.state.seconds} Second`
+            }`,
+            align: "center",
+          }}
+          position={{ x: 0, y: 2.5, z: -1 }}
+        />
+        <Entity
+          text={{
+            value: `You have Completed the challenge at ${this.state.minuteComp} Minute and ${
+              this.state.secondComp > 10
+                ? `${this.state.secondComp} Seconds`
+                : `${this.state.secondComp} Second`
+            }`,
+            align: "center",
+          }}
+          position={{ x: 0, y: 2.3, z: -1 }}
+        />
+        <Entity
+          text={{
+            value: `Find the odd ones  `,
+            align: "center",
+          }}
+          position={{ x: 0, y: 2, z: -1 }}
+        />
 
+        {/* Main shit */}
+        <Entity
+          geometry={{ primitive: "circle" }}
+          material={{ color: "red", roughness: 0.5 }}
+          position={{ x: 0, y: 2, z: -1 }}
+          events={{ click: this.timerStart.bind(this) }}
+        />
+        { this.state.succeed ? " ":
+        
+        this.state.displayData}
+        {this.state.toFind}
+
+        {/* Below is the camera */}
         <Entity primitive="a-camera">
-          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
+          <Entity
+            primitive="a-cursor"
+            animation__click={{
+              property: "scale",
+              startEvents: "click",
+              from: "0.1 0.1 0.1",
+              to: "1 1 1",
+              dur: 150,
+            }}
+          />
         </Entity>
       </Scene>
     );
   }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#sceneContainer'));
+ReactDOM.render(<App />, document.querySelector("#sceneContainer"));
